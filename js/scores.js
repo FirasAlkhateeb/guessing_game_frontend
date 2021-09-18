b75ded3903b2f6c2bfbd28b55a933ddb950afe96
FirasAlkhateeb <88935716+FirasAlkhateeb@users.noreply.github.com>
2021-09-18 16:17:50 +0300

    Update scores.js

--- js/scores.js
+++ js/scores.js
@@ -2,5 +2,11 @@
 //which fetch all names and scores from database
 
 getScores().then((data)=>{
-    console.log(data)
+console.log(data)
+for(let i=0 ;i<data.length; i++){
+    display += ` <li class="score-element">
+        <span>${data[i].name} </span>
+        <span>${data[i].score} </span>
+    </li> `
+    }
 })
